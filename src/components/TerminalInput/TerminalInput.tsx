4e38a14db3a18fe57d18741064c0386d56232ee6
Igor Šula <sulaigor@gmail.com>
2021-03-04 23:24:03 +0100

    Add adding terminal rows in terminal input component

--- src/components/TerminalInput/TerminalInput.tsx
+++ src/components/TerminalInput/TerminalInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, RefObject, useLayoutEffect } from 'react';
+import React, { ChangeEvent, FormEvent, RefObject, useLayoutEffect } from 'react';
 import { useTerminalContext } from 'contexts/TerminalContext';
 import TerminalLabel from 'components/TerminalLabel';
 import css from './terminalInput.module.scss';
@@ -8,25 +8,31 @@ interface IProps {
 }
 
 const TerminalInput = ({ inputRef }: IProps) => {
-  const { inputValue, setInputValue } = useTerminalContext();
+  const { inputValue, setInputValue, addTerminalRow } = useTerminalContext();
 
   useLayoutEffect(() => {
     inputRef.current?.focus();
   }, [inputRef]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value);
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addTerminalRow();
+  };
 
   return (
     <div className={css.terminalInputWrapper}>
       <TerminalLabel />
-      <input
-        spellCheck={false}
-        ref={inputRef}
-        className={css.terminalInput}
-        type='text'
-        value={inputValue}
-        onChange={handleInputChange}
-      />
+      <form className={css.inputForm} onSubmit={handleFormSubmit}>
+        <input
+          spellCheck={false}
+          ref={inputRef}
+          className={css.terminalInput}
+          type='text'
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
 };

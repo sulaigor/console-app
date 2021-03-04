b8e481d52154f53464a9f4069127d6c81124a3f9
Igor Šula <sulaigor@gmail.com>
2021-03-04 23:17:34 +0100

    Add terminal rows value to terminal reducer

--- src/contexts/TerminalContext/terminalReducer/const.ts
+++ src/contexts/TerminalContext/terminalReducer/const.ts
@@ -1,7 +1,9 @@
 import { IReducerState } from './types';
 
 export const DEFAULT_INPUT_VALUE = '';
+export const DEFAULT_TERMINAL_ROWS = [];
 
 export const initialState: IReducerState = {
   inputValue: DEFAULT_INPUT_VALUE,
+  terminalRows: DEFAULT_TERMINAL_ROWS,
 };
--- src/contexts/TerminalContext/terminalReducer/types.ts
+++ src/contexts/TerminalContext/terminalReducer/types.ts
@@ -1,3 +1,6 @@
+import { ITerminalRow } from 'types/terminal';
+
 export interface IReducerState {
   inputValue: string;
+  terminalRows: ITerminalRow[];
 }

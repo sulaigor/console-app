e3abe6c514b8ba1f9e4632d9c869fc8a1ae81a6f
Igor Šula <sulaigor@gmail.com>
2021-03-04 22:03:47 +0100

    Refactor context provider component

--- src/core/ContextProvider/ContextProvider.tsx
+++ src/core/ContextProvider/ContextProvider.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { IChildrenProps as IProps } from 'types/props';
 import TerminalContext from 'contexts/TerminalContext';
 
-function ContextProvider({ children }: IProps) {
+const ContextProvider = ({ children }: IProps) => {
   return <TerminalContext>{children}</TerminalContext>;
-}
+};
 
 export default ContextProvider;

80b651e8efd0187e636faef6427e72cdd5dad74d
Igor Šula <sulaigor@gmail.com>
2021-03-04 21:36:53 +0100

    Add reset input value method to terminal context

--- src/contexts/TerminalContext/TerminalContext.tsx
+++ src/contexts/TerminalContext/TerminalContext.tsx
@@ -1,22 +1,25 @@
 import React, { createContext, useContext, useReducer } from 'react';
 import { IChildrenProps as IProps } from 'types/props';
 import terminalReducer, { initialState } from './terminalReducer';
-import { setInputValueAction } from './terminalReducer/actions';
+import { resetInputValueAction, setInputValueAction } from './terminalReducer/actions';
 import { TerminalContextType } from './types';
 
 const Context = createContext<TerminalContextType>({
   ...initialState,
   setInputValue: () => null,
+  resetInputValue: () => null,
 });
 
 const TerminalContext = ({ children }: IProps) => {
   const [reducerState, dispatch] = useReducer(terminalReducer, initialState);
 
   const setInputValue = (newValue: string) => dispatch(setInputValueAction(newValue));
+  const resetInputValue = () => dispatch(resetInputValueAction());
 
   const contextValue: TerminalContextType = {
     ...reducerState,
     setInputValue,
+    resetInputValue,
   };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
--- src/contexts/TerminalContext/types.ts
+++ src/contexts/TerminalContext/types.ts
@@ -2,6 +2,7 @@ import { IReducerState } from './terminalReducer';
 
 interface ITerminalContextActions {
   setInputValue: (newValue: string) => void;
+  resetInputValue: () => void;
 }
 
 export type TerminalContextType = IReducerState & ITerminalContextActions;

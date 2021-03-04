108e31ef7ab896174661ca39c6889a9a4746c6d4
Igor Šula <sulaigor@gmail.com>
2021-03-04 23:19:39 +0100

    Add terminal row action to terminal reducer

--- src/contexts/TerminalContext/terminalReducer/actions.ts
+++ src/contexts/TerminalContext/terminalReducer/actions.ts
@@ -1,13 +1,17 @@
 import { IReducerAction } from 'types/reducer';
-import { RESET_INPUT_VALUE, SET_INPUT_VALUE } from './actionTypes';
+import { ITerminalRow } from 'types/terminal';
+import { ADD_TERMINAL_ROW, RESET_INPUT_VALUE, SET_INPUT_VALUE } from './actionTypes';
 
 export const setInputValueAction = (newValue: string): IReducerAction => ({
   type: SET_INPUT_VALUE,
-  payload: {
-    newValue,
-  },
+  payload: { newValue },
 });
 
 export const resetInputValueAction = (): IReducerAction => ({
   type: RESET_INPUT_VALUE,
+});
+
+export const addTerminalRowAction = (newRow: ITerminalRow): IReducerAction => ({
+  type: ADD_TERMINAL_ROW,
+  payload: { newRow },
 });

6092ff6477e38ed16850c1845f3eb27ac55c4c83
Igor Šula <sulaigor@gmail.com>
2021-03-10 22:49:05 +0100

    Add get not found output util function in commands module

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,4 +1,6 @@
-import { CLEAR_COMMAND, IN_COMMAND_SEPARATOR } from 'const/commands';
+import { CLEAR_COMMAND, IN_COMMAND_SEPARATOR, NOT_FOUND_OUTPUT } from 'const/commands';
 
-export const getCommandValue = (inputValue: string): string => inputValue.split(IN_COMMAND_SEPARATOR)[0];
-export const isClearCommand = (inputValue: string): boolean => getCommandValue(inputValue) === CLEAR_COMMAND;
+export const getCommand = (input: string): string => input.split(IN_COMMAND_SEPARATOR)[0];
+export const isClearCommand = (input: string): boolean => getCommand(input) === CLEAR_COMMAND;
+export const getNotFoundOutput = (input: string): string =>
+  input && `${NOT_FOUND_OUTPUT} ${getCommand(input)}`;

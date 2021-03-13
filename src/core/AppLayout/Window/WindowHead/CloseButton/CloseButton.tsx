2209fc509ebebf8fd99a2c0d6b24c7d35ba93956
Igor Šula <sulaigor@gmail.com>
2021-03-13 20:16:47 +0100

    Add close button component

---
+++ src/core/AppLayout/Window/WindowHead/CloseButton/CloseButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classnames from 'classnames';
+import css from './closeButton.module.scss';
+
+interface IProps {
+  onClick: () => void;
+  className?: string;
+}
+
+const CloseButton = ({ className, onClick }: IProps) => {
+  return (
+    <button type='button' className={classnames(css.closeBtn, className)} onClick={onClick}>
+      close button
+    </button>
+  );
+};
+
+export default CloseButton;

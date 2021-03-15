e566aa0f26c6a59fd4f4a283515016d161947929
Igor Šula <sulaigor@gmail.com>
2021-03-15 02:40:18 +0100

    Add terminal history saving and loading funcionality to terminal context

--- src/contexts/TerminalContext/TerminalContext.tsx
+++ src/contexts/TerminalContext/TerminalContext.tsx
@@ -1,15 +1,19 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useEffect, useReducer } from 'react';
 import { IChildrenProps as IProps } from 'types/props';
 import { TerminalRow } from 'models/TerminalRow';
 import { isClearCommand } from 'utils/commands';
 import { dummyFunction } from 'utils/dummy';
+import { setItem, loadItem } from 'utils/localStorage';
 import terminalReducer, { initialState } from './terminalReducer';
 import {
+  addTerminalHistoryAction,
   addTerminalRowAction,
   clearTerminalAction,
   resetInputValueAction,
   setInputValueAction,
+  setTerminalHistoryAction,
 } from './terminalReducer/actions';
+import { TERMINAL_HISTORY_KEY } from './const';
 import { TerminalContextType } from './types';
 
 const Context = createContext<TerminalContextType>({
@@ -21,10 +25,20 @@ const Context = createContext<TerminalContextType>({
 
 const TerminalContext = ({ children }: IProps) => {
   const [reducerState, dispatch] = useReducer(terminalReducer, initialState);
+  const { terminalHistory } = reducerState;
 
+  useEffect(() => loadTerminalHistory(), []);
+  useEffect(() => setItem(TERMINAL_HISTORY_KEY, JSON.stringify(terminalHistory)), [terminalHistory]);
+
+  const setHistory = (newTerminalHistory: string[]) => dispatch(setTerminalHistoryAction(newTerminalHistory));
   const setInputValue = (newValue: string) => dispatch(setInputValueAction(newValue));
   const resetInputValue = () => dispatch(resetInputValueAction());
   const clearTerminal = () => dispatch(clearTerminalAction());
+  const addTerminalHistory = (newHistoryItem: string) => {
+    if (newHistoryItem) {
+      dispatch(addTerminalHistoryAction(newHistoryItem));
+    }
+  };
 
   const addTerminalRow = () => {
     const { inputValue } = reducerState;
@@ -32,9 +46,18 @@ const TerminalContext = ({ children }: IProps) => {
     if (isClearCommand(inputValue)) clearTerminal();
     else {
       const terminalRow = new TerminalRow(inputValue);
+      const { command } = terminalRow;
+      addTerminalHistory(command);
       dispatch(addTerminalRowAction(terminalRow));
     }
     resetInputValue();
+  };
+
+  const loadTerminalHistory = () => {
+    const historySaveCallback = (savedTerminalHistory: string) =>
+      setHistory(JSON.parse(savedTerminalHistory));
+
+    loadItem(TERMINAL_HISTORY_KEY, historySaveCallback);
   };
 
   const contextValue: TerminalContextType = {

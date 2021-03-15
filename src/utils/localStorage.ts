42a99ff91c9f1e8ff920ee118d9fa7b80e2c6070
Igor Šula <sulaigor@gmail.com>
2021-03-15 02:39:16 +0100

    Add load item method to local storage utils

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,7 +1,7 @@
 const getEncodedData = (data: string) => btoa(data);
 const getDecodedData = (data: string) => atob(data);
 
-const getItem = (key: string): string => {
+export const getItem = (key: string): string => {
   const encodedKey = getEncodedData(key);
   const keyValue = localStorage.getItem(encodedKey);
 
@@ -9,13 +9,21 @@ const getItem = (key: string): string => {
   return getDecodedData(keyValue);
 };
 
-const setItem = (key: string, value: string): void => {
+export const loadItem = (key: string, saveCallback: (itemValue: string) => void) => {
+  const savedItem = getItem(key);
+
+  if (savedItem) {
+    saveCallback(savedItem);
+  }
+};
+
+export const setItem = (key: string, value: string): void => {
   const encodedKey = getEncodedData(key);
   const encodedValue = getEncodedData(value);
   localStorage.setItem(encodedKey, encodedValue);
 };
 
-const removeItem = (key: string): void => {
+export const removeItem = (key: string): void => {
   const encodedKey = getEncodedData(key);
   localStorage.removeItem(encodedKey);
 };

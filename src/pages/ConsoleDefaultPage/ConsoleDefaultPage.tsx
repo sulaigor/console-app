80afbce195f34e9edd2b76cf7c44e2d6a790c120
Igor Šula <sulaigor@gmail.com>
2021-03-30 00:52:45 +0200

    Add usage of use terminal history hook in console default page component

--- src/pages/ConsoleDefaultPage/ConsoleDefaultPage.tsx
+++ src/pages/ConsoleDefaultPage/ConsoleDefaultPage.tsx
@@ -1,10 +1,12 @@
 import React, { useRef } from 'react';
 import { useTerminalClear } from 'hooks/useTerminalClear';
+import { useTerminalHistory } from 'hooks/useTerminalHistory';
 import TerminalInput from 'components/TerminalInput';
 import TerminalRows from 'components/TerminalRows';
 
 const ConsoleDefaultPage = () => {
   useTerminalClear();
+  useTerminalHistory();
   const terminalInputRef = useRef<HTMLInputElement>(null);
 
   const handleTerminalClick = () => terminalInputRef.current?.focus();

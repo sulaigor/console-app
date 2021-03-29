1fbfa53d117b340d855ff258bf1c4075b85f21c9
Igor Šula <sulaigor@gmail.com>
2021-03-30 00:49:12 +0200

    Add new terminal reducer actions

--- src/contexts/TerminalContext/terminalReducer/actions.ts
+++ src/contexts/TerminalContext/terminalReducer/actions.ts
@@ -1,17 +1,17 @@
 import { IReducerAction } from 'types/reducer';
 import { ITerminalRow } from 'models/TerminalRow';
+import { HistoryIndexType, InputValueType } from './types';
 import {
-  ADD_TERMINAL_HISTORY,
+  ADD_TERMINAL_HISTORY_ITEM,
   ADD_TERMINAL_ROW,
   CLEAR_TERMINAL,
-  DECREASE_HISTORY_INDEX,
-  INCREASE_HISTORY_INDEX,
   RESET_INPUT_VALUE,
+  SET_HISTORY_INDEX,
   SET_INPUT_VALUE,
   SET_TERMINAL_HISTORY,
 } from './actionTypes';
 
-export const setInputValueAction = (newValue: string): IReducerAction => ({
+export const setInputValueAction = (newValue: InputValueType): IReducerAction => ({
   type: SET_INPUT_VALUE,
   payload: { newValue },
 });
@@ -29,20 +29,17 @@ export const clearTerminalAction = (): IReducerAction => ({
   type: CLEAR_TERMINAL,
 });
 
-export const setTerminalHistoryAction = (newTerminalHistory: string[]): IReducerAction => ({
+export const addTerminalHistoryItem = (newHistoryItem: InputValueType): IReducerAction => ({
+  type: ADD_TERMINAL_HISTORY_ITEM,
+  payload: { newHistoryItem },
+});
+
+export const setTerminalHistoryAction = (newTerminalHistory: InputValueType[]): IReducerAction => ({
   type: SET_TERMINAL_HISTORY,
   payload: { newTerminalHistory },
 });
 
-export const addTerminalHistoryAction = (newItem: string): IReducerAction => ({
-  type: ADD_TERMINAL_HISTORY,
-  payload: { newItem },
-});
-
-export const increaseHistoryIndexAction = (): IReducerAction => ({
-  type: INCREASE_HISTORY_INDEX,
-});
-
-export const decreaseHistoryIndexAction = (): IReducerAction => ({
-  type: DECREASE_HISTORY_INDEX,
+export const setHistoryIndexAction = (newIndex: HistoryIndexType): IReducerAction => ({
+  type: SET_HISTORY_INDEX,
+  payload: { newIndex },
 });

1743a57827657d7588cb0df3c0fe3bd9de9c9f09
Igor Šula <sulaigor@gmail.com>
2021-04-07 20:54:55 +0200

    Refactor use resize event custom hook

--- src/core/AppLayout/Window/hooks.ts
+++ src/core/AppLayout/Window/hooks.ts
@@ -1,7 +1,5 @@
 import { useLayoutEffect, useRef } from 'react';
 
-const RESIZE_EVENT = 'custom-resize';
-
 export const useResizeEvent = <T extends HTMLElement>() => {
   const elemRef = useRef<T>(null);
 
@@ -9,25 +7,15 @@ export const useResizeEvent = <T extends HTMLElement>() => {
     const { current: elem } = elemRef;
     if (!elem?.firstChild) return () => null;
 
-    const checkResize: ResizeObserverCallback = (mutations) => {
-      const observedElem = mutations[0].target;
-      const event = new CustomEvent(RESIZE_EVENT);
-      observedElem.dispatchEvent(event);
-    };
-
-    const childElem = elem.firstChild as Element;
-    const observer = new ResizeObserver(checkResize);
-    observer.observe(childElem);
-
-    const resizeHandler = () => {
+    const resizeCallback: ResizeObserverCallback = () => {
       elem.scrollTop = elem.scrollHeight;
     };
 
-    childElem.addEventListener(RESIZE_EVENT, resizeHandler);
-    return () => {
-      childElem.removeEventListener(RESIZE_EVENT, resizeHandler);
-      observer.disconnect();
-    };
+    const childElem = elem.firstChild as Element;
+    const observer = new ResizeObserver(resizeCallback);
+    observer.observe(childElem);
+
+    return () => observer.disconnect();
   }, [elemRef.current]);
 
   return elemRef;

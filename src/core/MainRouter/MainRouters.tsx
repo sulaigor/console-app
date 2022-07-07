30a9f124e9ec2f3afd565ff446b7ea0fa9c556af
Igor Šula <sulaigor@gmail.com>
2022-07-07 20:53:57 +0200

    Update react router dom

--- src/core/MainRouter/MainRouters.tsx
+++ src/core/MainRouter/MainRouters.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DEFAULT_PATH } from 'const/pagePaths';
 import ConsoleDefaultPage from 'pages/ConsoleDefaultPage';
 
 const MainRouters = () => {
   return (
     <Router>
-      <Switch>
-        <Route path={DEFAULT_PATH}>
-          <ConsoleDefaultPage />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path={DEFAULT_PATH} element={<ConsoleDefaultPage />} />
+      </Routes>
     </Router>
   );
 };

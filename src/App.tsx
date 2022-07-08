4b7ce06eea8891e84d1be2f2ad03a8fbbb5e0e4f
Igor Šula <sulaigor@gmail.com>
2022-07-09 01:09:12 +0200

    Add store provider to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import ContextProvider from 'core/ContextProvider';
 import AppLayout from 'core/AppLayout';
 import MainRouter from 'core/MainRouter';
+import StoreProvider from 'core/StoreProvider';
 
 const App = () => {
   return (
-    <ContextProvider>
-      <AppLayout>
-        <MainRouter />
-      </AppLayout>
-    </ContextProvider>
+    <StoreProvider>
+      <ContextProvider>
+        <AppLayout>
+          <MainRouter />
+        </AppLayout>
+      </ContextProvider>
+    </StoreProvider>
   );
 };
 

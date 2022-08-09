f7d201d4ceb1333b170acadbbed7bcd11005ccd2
Igor Šula <sulaigor@gmail.com>
2022-08-09 23:32:19 +0200

    Add terminal sagas action types

--- src/store/namespaces/terminal/actions/actionTypes.ts
+++ src/store/namespaces/terminal/actions/actionTypes.ts
@@ -1,11 +1,29 @@
-import { createTerminalReducerActionType } from './utils';
+import { createTerminalReducerActionType, createTerminalSagaActionType } from './utils';
+
+const SET_INPUT_VALUE = 'SET_INPUT_VALUE';
+const RESET_INPUT_VALUE = 'RESET_INPUT_VALUE';
+const ADD_TERMINAL_ROW = 'ADD_TERMINAL_ROW';
+const CLEAR_TERMINAL = 'CLEAR_TERMINAL';
+const ADD_TERMINAL_HISTORY_ITEM = 'ADD_TERMINAL_HISTORY_ITEM';
+const SET_TERMINAL_HISTORY = 'SET_TERMINAL_HISTORY';
+const SET_HISTORY_INDEX = 'SET_HISTORY_INDEX';
 
 export const ReducerActionTypes = {
-  SET_INPUT_VALUE: createTerminalReducerActionType('SET_INPUT_VALUE'),
-  RESET_INPUT_VALUE: createTerminalReducerActionType('RESET_INPUT_VALUE'),
-  ADD_TERMINAL_ROW: createTerminalReducerActionType('ADD_TERMINAL_ROW'),
-  CLEAR_TERMINAL: createTerminalReducerActionType('CLEAR_TERMINAL'),
-  ADD_TERMINAL_HISTORY_ITEM: createTerminalReducerActionType('ADD_TERMINAL_HISTORY_ITEM'),
-  SET_TERMINAL_HISTORY: createTerminalReducerActionType('SET_TERMINAL_HISTORY'),
-  SET_HISTORY_INDEX: createTerminalReducerActionType('SET_HISTORY_INDEX'),
+  SET_INPUT_VALUE: createTerminalReducerActionType(SET_INPUT_VALUE),
+  RESET_INPUT_VALUE: createTerminalReducerActionType(RESET_INPUT_VALUE),
+  ADD_TERMINAL_ROW: createTerminalReducerActionType(ADD_TERMINAL_ROW),
+  CLEAR_TERMINAL: createTerminalReducerActionType(CLEAR_TERMINAL),
+  ADD_TERMINAL_HISTORY_ITEM: createTerminalReducerActionType(ADD_TERMINAL_HISTORY_ITEM),
+  SET_TERMINAL_HISTORY: createTerminalReducerActionType(SET_TERMINAL_HISTORY),
+  SET_HISTORY_INDEX: createTerminalReducerActionType(SET_HISTORY_INDEX),
+};
+
+export const SagasActionTypes = {
+  SET_INPUT_VALUE: createTerminalSagaActionType(SET_INPUT_VALUE),
+  RESET_INPUT_VALUE: createTerminalSagaActionType(RESET_INPUT_VALUE),
+  ADD_TERMINAL_ROW: createTerminalSagaActionType(ADD_TERMINAL_ROW),
+  CLEAR_TERMINAL: createTerminalSagaActionType(CLEAR_TERMINAL),
+  ADD_TERMINAL_HISTORY_ITEM: createTerminalSagaActionType(ADD_TERMINAL_HISTORY_ITEM),
+  SET_TERMINAL_HISTORY: createTerminalSagaActionType(SET_TERMINAL_HISTORY),
+  SET_HISTORY_INDEX: createTerminalSagaActionType(SET_HISTORY_INDEX),
 };
--- src/store/namespaces/terminal/actions/utils.ts
+++ src/store/namespaces/terminal/actions/utils.ts
@@ -1,8 +1,10 @@
 import { createActionType } from 'utils/reducer';
 import { TERMINAL_REDUCER_NAME, TERMINAL_SAGA_NAME } from '../const';
 
+// Create action type with terminal reducer prefix
 export const createTerminalReducerActionType = (actionType: string): string =>
   createActionType(TERMINAL_REDUCER_NAME, actionType);
 
+// Create action type with terminal sagas prefix
 export const createTerminalSagaActionType = (actionType: string): string =>
   createActionType(TERMINAL_SAGA_NAME, actionType);

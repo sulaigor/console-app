ef58c8f763d4411c166ff9a63e62d3f5fddebef7
Igor Šula <sulaigor@gmail.com>
2022-11-16 23:06:44 +0100

    Edit terminal sagas actions

--- src/store/namespaces/terminal/actions/actionTypes.ts
+++ src/store/namespaces/terminal/actions/actionTypes.ts
@@ -6,20 +6,12 @@ import {
   ADD_TERMINAL_HISTORY_ITEM,
   SET_TERMINAL_HISTORY,
   SET_HISTORY_INDEX,
+  DECREASE_HISTORY_INDEX,
+  INCREASE_HISTORY_INDEX,
 } from './const';
 import { createTerminalReducerActionType, createTerminalSagaActionType } from './utils';
 
 export const ReducerActionTypes = {
-  SET_INPUT_VALUE: createTerminalReducerActionType(SET_INPUT_VALUE),
-  RESET_INPUT_VALUE: createTerminalReducerActionType(RESET_INPUT_VALUE),
-  ADD_TERMINAL_ROW: createTerminalReducerActionType(ADD_TERMINAL_ROW),
-  CLEAR_TERMINAL: createTerminalReducerActionType(CLEAR_TERMINAL),
-  ADD_TERMINAL_HISTORY_ITEM: createTerminalReducerActionType(ADD_TERMINAL_HISTORY_ITEM),
-  SET_TERMINAL_HISTORY: createTerminalReducerActionType(SET_TERMINAL_HISTORY),
-  SET_HISTORY_INDEX: createTerminalReducerActionType(SET_HISTORY_INDEX),
-};
-
-export const SagasActionTypes = {
   SET_INPUT_VALUE: createTerminalSagaActionType(SET_INPUT_VALUE),
   RESET_INPUT_VALUE: createTerminalSagaActionType(RESET_INPUT_VALUE),
   ADD_TERMINAL_ROW: createTerminalSagaActionType(ADD_TERMINAL_ROW),
@@ -27,4 +19,9 @@ export const SagasActionTypes = {
   ADD_TERMINAL_HISTORY_ITEM: createTerminalSagaActionType(ADD_TERMINAL_HISTORY_ITEM),
   SET_TERMINAL_HISTORY: createTerminalSagaActionType(SET_TERMINAL_HISTORY),
   SET_HISTORY_INDEX: createTerminalSagaActionType(SET_HISTORY_INDEX),
+};
+
+export const SagasActionTypes = {
+  INCREASE_HISTORY_INDEX: createTerminalReducerActionType(INCREASE_HISTORY_INDEX),
+  DECREASE_HISTORY_INDEX: createTerminalReducerActionType(DECREASE_HISTORY_INDEX),
 };
--- src/store/namespaces/terminal/actions/const.ts
+++ src/store/namespaces/terminal/actions/const.ts
@@ -1,7 +1,7 @@
 export const TERMINAL_REDUCER_NAME = '@terminalReducer';
 export const TERMINAL_SAGA_NAME = '@terminalSaga';
 
-// Used actions
+// Used reducer actions
 export const SET_INPUT_VALUE = 'SET_INPUT_VALUE';
 export const RESET_INPUT_VALUE = 'RESET_INPUT_VALUE';
 export const ADD_TERMINAL_ROW = 'ADD_TERMINAL_ROW';
@@ -9,3 +9,7 @@ export const CLEAR_TERMINAL = 'CLEAR_TERMINAL';
 export const ADD_TERMINAL_HISTORY_ITEM = 'ADD_TERMINAL_HISTORY_ITEM';
 export const SET_TERMINAL_HISTORY = 'SET_TERMINAL_HISTORY';
 export const SET_HISTORY_INDEX = 'SET_HISTORY_INDEX';
+
+// Used sagas actions
+export const INCREASE_HISTORY_INDEX = 'INCREASE_HISTORY_INDEX';
+export const DECREASE_HISTORY_INDEX = 'DECREASE_HISTORY_INDEX';
